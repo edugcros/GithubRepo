4a66af7dcc237954d67aa316e364bf4ea4c26337
edugcros <grecoeduardo87@gmail.com>
2024-04-29 13:09:00 -0300

    change

--- src/components/DisplayTable.js
+++ src/components/DisplayTable.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const DisplayTable = ({ data, repositories, username }) => {
-  const [content, setContent] = useState();
+  const [contents, setContents] = useState();
   const navigate = useNavigate();
 
   const fetchContent = async () => {
@@ -11,13 +11,9 @@ const DisplayTable = ({ data, repositories, username }) => {
       `https://api.github.com/repos/${username}/${repositories}/contents`
     );
     const contentJson = await content.json();
-    setContent(contentJson);
-    console.log(setContent(content));
-  };
-
-  const handleNavigate = () => {
-    fetchContent();
-    navigate("/repo", { state: { content } });
+    setContents(contentJson);
+    console.log(contents);
+    navigate("/repo", { state: { contents } });
   };
 
   return (
@@ -53,7 +49,7 @@ const DisplayTable = ({ data, repositories, username }) => {
                 <div className="item">
                   <i className="large github middle aligned icon"></i>
                   <div className="content">
-                    <Link className="headerBtn" onClick={handleNavigate}>
+                    <Link className="headerBtn" onClick={fetchContent}>
                       {repo.name}
                     </Link>
                   </div>
